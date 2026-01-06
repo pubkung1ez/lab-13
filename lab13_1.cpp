5548bec6f2deb576bda5672f3c5e82d4dcacc72a
pubkung1ez <tirapan_30506@sysp.ac.th>
2026-01-06 17:23:55 +0700

    suss

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,35 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double Au[],int Nu,double bu[]){
+    double sum = 0;
+    double ssum = 0;
+    double sssum = 1;
+    double ssssum = 0;
+    double max = 0;
+    double min = Au[0];
+    for(int i =0;i <Nu;i++){
+        sum += Au[i];
+        sssum *= Au[i];
+        ssssum += 1/Au[i];
+    }
+    double amean = sum/Nu;
+    bu[0] = amean;
+    for(int j = 0;j < Nu;j++){
+        ssum += pow(Au[j]-amean,2);
+    }
+    double sde = ssum/Nu;
+    bu[1] = sqrt(sde);
+    double gem = pow(sssum,1.0/Nu);
+    bu[2] = gem;
+    double ham = Nu/ssssum;
+    bu[3] = ham;
+    for(int k=0;k<Nu;k++){
+        if(Au[k] > max) max = Au[k];
+        if(Au[k] < min) min = Au[k];
+    }
+    bu[4] = max;
+    bu[5] = min;
+    
+}
